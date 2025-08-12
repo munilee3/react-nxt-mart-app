1dbe0b1d33b67892c2047e9a56a2379ed2048b9e
muniswamy <muniswamykanike@gmail.com>
2025-08-12 15:26:13 +0000

    testing test cases

--- src/App.js
+++ src/App.js
@@ -11,103 +11,88 @@ import AppContext from './context/AppContext'
 import './App.css'
 
 class App extends Component {
-  state = {
-    cartList: '',
-    activeTab: '',
-    activeCategory: '',
-  }
+  // state = {
+  //   cartList: '',
+  //   activeTab: '',
+  //   activeCategory: '',
+  // }
 
-  componentDidMount = () => {
-    const getItemsFromLocalStorate =
-      JSON.parse(localStorage.getItem('CartItems')) || []
-    this.setState({cartList: getItemsFromLocalStorate})
-  }
+  // componentDidMount = () => {
+  //   const getItemsFromLocalStorate =
+  //     JSON.parse(localStorage.getItem('CartItems')) || []
+  //   this.setState({cartList: getItemsFromLocalStorate})
+  // }
 
-  changeTab = id => {
-    this.setState({activeTab: id})
-  }
+  // changeTab = id => {
+  //   this.setState({activeTab: id})
+  // }
 
-  changeCategory = id => {
-    this.setState({activeCategory: id})
-  }
+  // changeCategory = id => {
+  //   this.setState({activeCategory: id})
+  // }
 
-  addCartItem = product => {
-    const newProduct = {...product, quantity: 1}
-    this.setState(prevState => {
-      const updatedCart = [...prevState.cartList, newProduct]
-      localStorage.setItem('CartItems', JSON.stringify(updatedCart))
-      return {cartList: updatedCart}
-    })
-  }
+  // addCartItem = product => {
+  //   const newProduct = {...product, quantity: 1}
+  //   this.setState(prevState => {
+  //     const updatedCart = [...prevState.cartList, newProduct]
+  //     localStorage.setItem('CartItems', JSON.stringify(updatedCart))
+  //     return {cartList: updatedCart}
+  //   })
+  // }
 
-  removeCartItem = removeItem => {
-    const {cartList} = this.state
-    const filteredList = cartList.filter(eachItem => eachItem.id !== removeItem)
-    localStorage.setItem('CartItems', JSON.stringify(filteredList))
-    this.setState({cartList: filteredList})
-  }
+  // removeCartItem = removeItem => {
+  //   const {cartList} = this.state
+  //   const filteredList = cartList.filter(eachItem => eachItem.id !== removeItem)
+  //   localStorage.setItem('CartItems', JSON.stringify(filteredList))
+  //   this.setState({cartList: filteredList})
+  // }
 
-  incrementCartItemQuantity = id => {
-    const {cartList} = this.state
-    const filteredList = cartList.map(eachItem => {
-      if (eachItem.id === id) {
-        return {...eachItem, quantity: eachItem.quantity + 1}
-      }
-      return eachItem
-    })
-    localStorage.setItem('CartItems', JSON.stringify(filteredList))
-    this.setState({cartList: filteredList})
-  }
+  // incrementCartItemQuantity = id => {
+  //   const {cartList} = this.state
+  //   const filteredList = cartList.map(eachItem => {
+  //     if (eachItem.id === id) {
+  //       return {...eachItem, quantity: eachItem.quantity + 1}
+  //     }
+  //     return eachItem
+  //   })
+  //   localStorage.setItem('CartItems', JSON.stringify(filteredList))
+  //   this.setState({cartList: filteredList})
+  // }
 
-  decrementCartItemQuantity = id => {
-    const {cartList} = this.state
-    const cartItem = cartList.filter(eachItem => eachItem.id === id)
-    const {quantity} = cartItem[0]
-    if (quantity > 1) {
-      const newCartList = cartList.map(eachItem => {
-        if (eachItem.id === id) {
-          return {...eachItem, quantity: eachItem.quantity - 1}
-        }
-        return eachItem
-      })
-      localStorage.setItem('CartItems', JSON.stringify(newCartList))
-      this.setState({cartList: newCartList})
-    } else {
-      this.removeCartItem(id)
-    }
-  }
+  // decrementCartItemQuantity = id => {
+  //   const {cartList} = this.state
+  //   const cartItem = cartList.filter(eachItem => eachItem.id === id)
+  //   const {quantity} = cartItem[0]
+  //   if (quantity > 1) {
+  //     const newCartList = cartList.map(eachItem => {
+  //       if (eachItem.id === id) {
+  //         return {...eachItem, quantity: eachItem.quantity - 1}
+  //       }
+  //       return eachItem
+  //     })
+  //     localStorage.setItem('CartItems', JSON.stringify(newCartList))
+  //     this.setState({cartList: newCartList})
+  //   } else {
+  //     this.removeCartItem(id)
+  //   }
+  // }
 
-  removeAllCartItems = () => {
-    this.setState({cartList: []})
-  }
+  // removeAllCartItems = () => {
+  //   this.setState({cartList: []})
+  // }
 
   render() {
-    const {cartList, activeTab, activeCategory} = this.state
+    // const {cartList, activeTab, activeCategory} = this.state
     return (
-      <AppContext.Provider
-        value={{
-          cartList,
-          activeTab,
-          activeCategory,
-          addCartItem: this.addCartItem,
-          removeCartItem: this.removeCartItem,
-          incrementCartItemQuantity: this.incrementCartItemQuantity,
-          decrementCartItemQuantity: this.decrementCartItemQuantity,
-          removeAllCartItems: this.removeAllCartItems,
-          changeTab: this.changeTab,
-          changeCategory: this.changeCategory,
-        }}
-      >
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/login" component={LoginForm} />
-            <ProtectedRoute exact path="/" component={Home} />
-            <ProtectedRoute exact path="/cart" component={Cart} />
-            <Route exact path="/not-found" component={NotFound} />
-            <Redirect to="/not-found" />
-          </Switch>
-        </BrowserRouter>
-      </AppContext.Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route exact path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
